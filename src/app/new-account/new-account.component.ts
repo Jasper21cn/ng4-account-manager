4f3d1b0f177fd1db3e9973fd45b357daddf95360
Jasper Cao <jaspersitacorp@gmail.com>
2017-12-08 16:51:27 -0500

    Initial commit

---
+++ src/app/new-account/new-account.component.ts
@@ -0,0 +1,22 @@
+import {Component, OnInit} from '@angular/core';
+import {AccountService} from '../account.service';
+
+@Component({
+  selector: 'app-new-account',
+  templateUrl: './new-account.component.html',
+  styleUrls: ['./new-account.component.css'],
+})
+export class NewAccountComponent implements OnInit {
+  constructor(private accountService: AccountService) {
+    this.accountService.statusUpdated.subscribe(
+      (status: string) => alert('New status: ' + status)
+    );
+  }
+
+  ngOnInit() {
+  }
+
+  onCreateAccount(accountName: string, accountStatus: string) {
+    this.accountService.addAccount(accountName, accountStatus);
+  }
+}
